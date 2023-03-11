b526b19386edfe3ac63258a8ed9ed9b15ee31208
Wenxin Sun <wsun210@gmail.com>
2023-03-11 23:27:25 +1300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function ClickButton() {
+  const [count, setCount] = useState(0);
+
+  const clickHandler = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  }
+
+  const resetCount = (event) => {
+    setCount(0);
+    event.stopPropagation();
+  }
+
+  return(
+    <div 
+      className="center" 
+      onClick={clickHandler}
+    >
+      <button onClick={resetCount}>Reset Count</button>
+      <p>
+        {count}
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
